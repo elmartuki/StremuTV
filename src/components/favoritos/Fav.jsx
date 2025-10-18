5dd9be59a7c4d94538edd5bf1bca21126016098f
Diaz Martin <elmartind17@gmail.com>
2025-10-18 14:11:39 -0300

    fix(verlcel):Se arreglo un bug en la importacion

--- src/components/favoritos/Fav.jsx
+++ src/components/favoritos/Fav.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { obtenerDelLocalStorage } from "../../utils/localStorage";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import close from "../../assets/close.svg";
-import ModalConfirm from "./modalConfirm";
+import ModalConfirmar from "./ModalConfirmar.jsx";
 
 export default function Fav({ onUpdateList, favoritos }) {
   const [listFavoritos, setListFavoritos] = useState([]);
@@ -35,7 +34,7 @@ export default function Fav({ onUpdateList, favoritos }) {
 
   return (
     <>
-      <ModalConfirm
+      <ModalConfirmar
         openModal={openDeleteModal}
         closeModal={cancelDelete}
         onConfirm={confirmDelete}
--- src/components/favoritos/ModalConfirm.jsx
+++ src/components/favoritos/ModalConfirm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ModalDelate from "../admin/ModalDelate";
 
-export default function ModalConfirm({ openModal, closeModal, onConfirm }) {
+export default function ModalConfirmar({ openModal, closeModal, onConfirm }) {
   return (
     <>
       <ModalDelate
