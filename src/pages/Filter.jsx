39d47576859559f74ce1bab2ac7de5a8693114b5
Diaz Martin <elmartind17@gmail.com>
2025-10-19 20:30:21 -0300

    refactor(ui): Se merjoro el diseño del filtrador

--- src/pages/Filter.jsx
+++ src/pages/Filter.jsx
@@ -1,102 +1,91 @@
 import React, { useState } from "react";
-import back from "../assets/back.svg";
 
-export default function Filter({ openModal, closeModal, generoObtenido }) {
+export default function Filter({ generoObtenido }) {
   const [genero, setGenero] = useState("");
 
-  if (openModal) {
-    return (
-      <section className="filter-section">
-        <article className="filter">
-          <div className="btn-access">
-            <button onClick={closeModal}>
-              <img src={back} alt="" />
-            </button>
+  return (
+    <article className="filter">
+      <div className="btn-access"></div>
+      <div className="filter-buttons">
+        <div className="modal-edit_inputs_buttons" role="radiogroup">
+          <div>
+            <input
+              type="radio"
+              name="genero"
+              id="accion"
+              value="Accion"
+              checked={genero === "Accion"}
+              onClick={() => generoObtenido(genero)}
+              onChange={(event) => setGenero(event.target.value)}
+            />
+            <label htmlFor="accion">Acción</label>
           </div>
-          <div className="filter-buttons">
-            <div className="modal-edit_inputs_buttons" role="radiogroup">
-              <div>
-                <input
-                  type="radio"
-                  name="genero"
-                  id="accion"
-                  value="Accion"
-                  checked={genero === "Accion"}
-                  onChange={(event) => setGenero(event.target.value)}
-                />
-                <label htmlFor="accion">Acción</label>
-              </div>
-              <div>
-                <input
-                  type="radio"
-                  name="genero"
-                  id="terror"
-                  value="Terror"
-                  checked={genero === "Terror"}
-                  onChange={(event) => setGenero(event.target.value)}
-                />
-                <label htmlFor="terror">Terror</label>
-              </div>
-
-              <div>
-                <input
-                  type="radio"
-                  name="genero"
-                  id="ciencia-ficcion"
-                  value="Ciencia Ficcion"
-                  checked={genero === "Ciencia Ficcion"}
-                  onChange={(event) => setGenero(event.target.value)}
-                />
-                <label htmlFor="ciencia-ficcion">Ciencia Ficcion</label>
-              </div>
-
-              <div>
-                <input
-                  type="radio"
-                  name="genero"
-                  id="Comedia"
-                  value="Comedia"
-                  checked={genero === "Comedia"}
-                  onChange={(event) => setGenero(event.target.value)}
-                />
-                <label htmlFor="Comedia">Comedia</label>
-              </div>
-
-              <div>
-                <input
-                  type="radio"
-                  name="genero"
-                  id="Drama"
-                  value="Drama"
-                  checked={genero === "Drama"}
-                  onChange={(event) => setGenero(event.target.value)}
-                />
-                <label htmlFor="Drama">Drama</label>
-              </div>
-
-              <div>
-                <input
-                  type="radio"
-                  name="genero"
-                  id="Fantasia"
-                  value="Fantasia"
-                  checked={genero === "Fantasia"}
-                  onChange={(event) => setGenero(event.target.value)}
-                />
-                <label htmlFor="Fantasia">Fantasia</label>
-              </div>
-            </div>
+          <div>
+            <input
+              type="radio"
+              name="genero"
+              id="terror"
+              value="Terror"
+              checked={genero === "Terror"}
+              onClick={() => generoObtenido(genero)}
+              onChange={(event) => setGenero(event.target.value)}
+            />
+            <label htmlFor="terror">Terror</label>
           </div>
 
-          <button
-            className="filter_buttons-btn"
-            type="button"
-            onClick={() => generoObtenido(genero)}
-          >
-            Aplicar
-          </button>
-        </article>
-      </section>
-    );
-  }
+          <div>
+            <input
+              type="radio"
+              name="genero"
+              id="ciencia-ficcion"
+              value="Ciencia Ficcion"
+              checked={genero === "Ciencia Ficcion"}
+              onClick={() => generoObtenido(genero)}
+              onChange={(event) => setGenero(event.target.value)}
+            />
+            <label htmlFor="ciencia-ficcion">Ciencia Ficcion</label>
+          </div>
+
+          <div>
+            <input
+              type="radio"
+              name="genero"
+              id="Comedia"
+              value="Comedia"
+              checked={genero === "Comedia"}
+              onClick={() => generoObtenido(genero)}
+              onChange={(event) => setGenero(event.target.value)}
+            />
+            <label htmlFor="Comedia">Comedia</label>
+          </div>
+
+          <div>
+            <input
+              type="radio"
+              name="genero"
+              id="Drama"
+              value="Drama"
+              checked={genero === "Drama"}
+              onClick={() => generoObtenido(genero)}
+              onChange={(event) => setGenero(event.target.value)}
+            />
+            <label htmlFor="Drama">Drama</label>
+          </div>
+
+          <div>
+            <input
+              type="radio"
+              name="genero"
+              id="Fantasia"
+              value="Fantasia"
+              checked={genero === "Fantasia"}
+              onClick={() => generoObtenido(genero)}
+              onChange={(event) => setGenero(event.target.value)}
+            />
+            <label htmlFor="Fantasia">Fantasia</label>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
 }
