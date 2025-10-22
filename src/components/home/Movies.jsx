dbf564a0bc9cf1b689dc21944b9af4b2da380e22
Diaz Martin <elmartind17@gmail.com>
2025-10-22 18:18:26 -0300

    Se arreglo un bug

--- src/components/home/Movies.jsx
+++ src/components/home/Movies.jsx
@@ -1,19 +1,32 @@
-import { obtenerPeliculasOSerieLS } from "../../utils/localStorage";
+import { useEffect } from "react";
+import {
+  agregarListado,
+  obtenerDelLocalStorage,
+  obtenerPeliculasOSerieLS,
+} from "../../utils/localStorage";
 import { NavLink } from "react-router-dom";
 
 export default function Movies() {
   const movieList = obtenerPeliculasOSerieLS("Pelicula") || [];
   const moviesRandom = movieList.sort(() => Math.random() - 0.5);
 
+  const [moviesList, setMovieList] = useState([]);
+  const [agregar, setAgregar] = useState();
+
+  useEffect(() => {
+    const agregar = agregarListado("MoviesSeries");
+    setAgregar(agregar);
+  }, []);
+
+  useEffect(() => {
+    const moviesList = obtenerDelLocalStorage("MoviesSeries");
+    setMovieList(moviesList);
+  }, []);
+
   return (
     <>
       {moviesRandom.map(({ id, nombre, url }) => (
-        <NavLink
-
-          to={`/pelicula/${id}`}
-          className="movies-card-home"
-          key={id}
-        >
+        <NavLink to={`/pelicula/${id}`} className="movies-card-home" key={id}>
           <div className="movies-card-home_img">
             <img src={url} alt={nombre} />
           </div>
