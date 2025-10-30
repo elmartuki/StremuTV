ac98b6ef30b7c0682f342b48dbf46274ce531213
Diaz Martin <elmartind17@gmail.com>
2025-10-29 23:18:44 -0300

    refactor(error-404): Se cambio el diseño de la pagima de error

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -21,6 +21,7 @@ import TermUse from "../pages/TermUsePage.jsx";
 import UsserRoutes from "./UsserRoutes.jsx";
 import AdminRoutes from "./AdminRoutes.jsx";
 import UssersPanel from "../components/ussers/UssersPanel.jsx";
+import CompatibleDevices from "../pages/CompatibleDevices.jsx";
 
 export default function AppRouter() {
   return (
@@ -104,6 +105,16 @@ export default function AppRouter() {
           element={
             <>
               <AccountModal />
+              <Footer />
+            </>
+          }
+        />
+
+        <Route
+          path="/dispositivos"
+          element={
+            <>
+              <CompatibleDevices />
               <Footer />
             </>
           }
